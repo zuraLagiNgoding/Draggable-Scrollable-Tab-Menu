fbf4909bb1a08e246ef2a3f90c7b93cf34eda0e7
zuraLagiNgoding <jibau19@gmail.com>
2023-05-27 21:18:15 +0700

    initial commit

--- main.js
+++ main.js
@@ -38,3 +38,26 @@ tabMenu.addEventListener("mousedown", () => {
     activeDrag = true;
 });
 
+//tab select
+
+const tabs = document.querySelectorAll(".tab");
+const tabBtns = document.querySelectorAll(".tab-btn");
+
+const tab_Nav = function (tabBtnClick) {
+    tabBtns.forEach((tabBtn) => {
+        tabBtn.classList.remove("active")
+    });
+    tabs.forEach((tab) => {
+        tab.classList.remove("active")
+    });
+
+    tabBtns[tabBtnClick].classList.add("active");
+    tabs[tabBtnClick].classList.add("active");
+}
+
+tabBtns.forEach((tabBtn, i) => {
+    tabBtn.addEventListener("click", () => {
+        tab_Nav(i);
+    });
+});
+
